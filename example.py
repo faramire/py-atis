e860e0904afab9457b6996bb816b4b1e0e7e2d4e
Fabio Ramirez Stern <github@faramire.de>
2024-01-15 00:10:02 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 # ========================================
 # 
-# Py-ATIS
+# Py-ATIS module
+# github.com/faramire/py-atis/
 # (c) Fabio Ramirez Stern, 2023-2024
 # 
 # ========================================
@@ -9,13 +10,13 @@
 
 owmAPIkey = 'YOUR-OWM-KEY' # OpenWeatherMap API key
 wxapikey = 'YOUR-WX-KEY' # checkWXapi key
-wxurl = 'https://api.checkwx.com/metar/XXXX/' # WX API url
+icaoCode = 'XXXX' # ICAO airport code
 
 atislocation = 'YOUR-LOCATION-STRING' # Location
 rwy = '25' # rwy to be said in ATIS, as string!
 expect = 'expect foot approach'
-lat =  '12.34'
-long = '56.78'
+lat = 12.34
+lon = 56.78
 
 # IMPORTS
 
@@ -26,12 +27,13 @@ from atis import createATIS()
 # SETUP
 
 letterc = 0 # Alpha
+wxurl = f'https://api.checkwx.com/metar/{icaoCode}/' # WX API url
 
 # ========================================
 
 # RUNTIME
 
-cATIS = createATIS(wxurl, wxapikey, owmAPIkey, atislocation, rwy, lat, long, letterc)
+cATIS = createATIS(wxurl_=wxurl, wxapikey_=wxapikey, owmAPIkey_=owmAPIkey, atislocation_=atislocation, rwy_=rwy, lat_=lat, lon_=lon, letterc_=letterc)
 
 print(cATIS[0])
 print("\n=============================\n")
